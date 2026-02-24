559f1f446de41298c0811ef1cc43e32ce844bb63
hanakhalda11-del <hanakhalda11@gmail.com>
2026-02-24 16:42:08 +0700

    c2

---
+++ c.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+bool cek = true;
+char jk;
+int angka = 18;
+float tinggi = 100.5;
+double harga = 200000.00;
+string nama = "Abra";
+
+jk = 'L';
+cout << "Selamat Datang" << endl;
+cout << "di TI UMY\n";
+cout << "Yougyakarta\n";
+
+nama = "Abra";
+cout << "Nama Saya : "<< nama;
+}
