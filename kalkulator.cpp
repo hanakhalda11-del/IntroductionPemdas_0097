aa4585b4f1bf55bd40e19bd5341b4f8bdb80d6fe
hanakhalda11-del <hanakhalda11@gmail.com>
2026-02-24 17:08:46 +0700

    c4

---
+++ kalkulator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int angka1, angka2, hasil;
+
+    cout << "=== kalkulator hana ===" << endl;
+
+    cout << "masukkan angka pertama: ";
+    cin >> angka1;
+
+    cout << "masukkan angka kedua: ";
+    cin >> angka2;
+
+    hasil = angka1 + angka2;
+
+    cout << "hasilnya: " << hasil << endl;
+    cout << "ily jeno" << endl;
+
+    return 0;
+}
